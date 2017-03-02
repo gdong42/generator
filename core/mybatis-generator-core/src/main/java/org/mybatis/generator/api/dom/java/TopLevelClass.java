d531f58191ad6fc1637f9e02c4acad24ba411493
Gan Dong <hotterd@gmail.com>
2017-03-02 08:28:38 +0800

    Fix array importing is not correct

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/TopLevelClass.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/TopLevelClass.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2016 the original author or authors.
+ *    Copyright 2006-2017 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -92,8 +92,20 @@ public class TopLevelClass extends InnerClass implements CompilationUnit {
                 && !importedType.getPackageName().equals(
                         getType().getPackageName())
                 && !importedType.getShortName().equals(getType().getShortName())) {
-            importedTypes.add(importedType);
+            if (importedType.isArray()) {
+                importedTypes.add(new FullyQualifiedJavaType(trimArraySuffix(importedType.getFullyQualifiedName())));
+            } else {
+                importedTypes.add(importedType);
+            }
         }
+    }
+
+    private String trimArraySuffix(String fullyQualifiedName) {
+        int index = fullyQualifiedName.lastIndexOf("[]");
+        if (index != -1) {
+            return fullyQualifiedName.substring(0, index);
+        }
+        return fullyQualifiedName;
     }
 
     /* (non-Javadoc)
